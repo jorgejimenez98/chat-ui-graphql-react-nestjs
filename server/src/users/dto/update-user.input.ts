b7a028545edd401a17d9c90ad1c834f626e0a7f8
Jorge Jimenez <georgeguitarra07@gmail.com>
2024-02-16 21:06:33 +0100

    feat(user-resource): Add crud for graphlq and mongoose for delete, update, create and queries

--- server/src/users/dto/update-user.input.ts
+++ server/src/users/dto/update-user.input.ts
@@ -1,9 +1,9 @@
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql'
+import { InputType, Field, PartialType } from '@nestjs/graphql'
 
 import { CreateUserInput } from './create-user.input'
 
 @InputType()
 export class UpdateUserInput extends PartialType(CreateUserInput) {
-  @Field(() => Int)
-  id: number
+  @Field()
+  _id: string
 }
